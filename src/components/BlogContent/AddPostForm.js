0d37fd8d8e2bce489b7a27ec69a02c03b332f2f7
Marat Mutalipov <maratios41@gmail.com>
2022-06-03 17:41:54 +0300

    made axios get request

--- src/components/BlogContent/AddPostForm.js
+++ src/components/BlogContent/AddPostForm.js
@@ -1,26 +1,90 @@
-import './AddPostForm.css'
+import React from 'react';
+import './AddPostForm.css';
 import CancelIcon from '@mui/icons-material/Cancel';
 
-export const AddPostForm = ({ handleAddFormHide }) => {
+export class AddPostForm extends React.Component {
+  state = {
+    postTitle: '',
+    postDesc: '',
+  };
+
+  handlePostTitleChange = (e) => {
+    this.setState({
+      postTitle: e.target.value,
+    });
+  };
+
+  handlePostDescChange = (e) => {
+    this.setState({
+      postDesc: e.target.value,
+    });
+  };
+
+  createPost = () => {
+      
+      const post = {
+        id: this.props.blogArr.length + 1,
+        title: this.state.postTitle,
+        description: this.state.postDesc,
+        liked: false,
+      }
+      this.props.addNewBlogPost(post)
+
+      this.props.handleAddFormHide()
+  }
+
+  handleSubmit = (e) => {
+        e.preventDefault()
+        if(e.keyCode == 13) {
+            this.createPost()
+        }
+    
+  }
+
+  componentDidMount() {
+      window.addEventListener('keyup', this.handleSubmit)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleSubmit)
+}
+
+  render() {
     return (
-        <>
-        <form action="" className="addPostform">
-            <h2>Создание поста</h2>
-            <button onClick={handleAddFormHide} className='hideBtn'>
-                <CancelIcon />
+      <>
+        <form action="" className="addPostform" onSubmit={this.createPost}>
+          <h2>Создание поста</h2>
+          <button onClick={this.props.handleAddFormHide} className="hideBtn">
+            <CancelIcon />
+          </button>
+          <div>
+            <input
+              onChange={this.handlePostTitleChange}
+              type="text"
+              name="postTitle"
+              value={this.state.postTitle} 
+              required
+            />
+          </div>
+          <div>
+            <textarea
+              onChange={this.handlePostDescChange}
+              name="postDescription"
+              id=""
+              cols="38"
+              rows="10"
+              value={this.state.postDesc} 
+              required
+            />
+          </div>
+          <div>
+            <button className="blackBtn" type="submit">
+              Добавить пост
             </button>
-            <div>
-                <input type='text' name='postTitle' />
-            </div>
-            <div>
-                <textarea name="postDescription" id="" cols="38" rows="10" />
-            </div>
-            <div>
-                <button onClick={handleAddFormHide} className="blackBtn" type="button">Добавить пост</button>
-            </div>
-            
+          </div>
         </form>
-        <div onClick={handleAddFormHide} className="overlay"></div>
-       </>
-    )
+        <div onClick={this.props.handleAddFormHide} className="overlay"></div>
+      </>
+    );
+  }
 }
--- src/components/BlogContent/BlogContent.js
+++ src/components/BlogContent/BlogContent.js
@@ -1,81 +1,109 @@
-import React from 'react'
-import './BlogContent.css'
-import { posts } from '../../shared/ProjectData'
-import BlogCard from './BlogCard'
-import { AddPostForm } from './AddPostForm'
+import React from 'react';
+import './BlogContent.css';
+import { posts } from '../../shared/ProjectData';
+import BlogCard from './BlogCard';
+import { AddPostForm } from './AddPostForm';
+import { SatelliteAlt } from '@mui/icons-material';
+import axios from 'axios';
 
-export class BlogContent extends React.Component  {
-
+export class BlogContent extends React.Component {
   state = {
     showAddForm: false,
-    blogArr: JSON.parse(localStorage.getItem('blogPosts')) || posts
-  }
+    blogArr: [],
+  };
 
   likePost = (pos) => {
-    const temp = [...this.state.blogArr]
-    temp[pos].liked = !temp[pos].liked
+    const temp = [...this.state.blogArr];
+    temp[pos].liked = !temp[pos].liked;
 
     this.setState({
-      blogArr: temp
-    })
+      blogArr: temp,
+    });
 
-    localStorage.setItem('blogPosts', JSON.stringify(temp))
-  }
-
-   
+    localStorage.setItem('blogPosts', JSON.stringify(temp));
+  };
 
   deletePost = (pos) => {
-    if(window.confirm(`Удалить ${this.state.blogArr[pos].title}?`)) {
-      const temp = [...this.state.blogArr]
-    temp.splice(pos, 1)
-    this.setState({
-      blogArr: temp
-    })
-    localStorage.setItem('blogPosts', JSON.stringify(temp))
+    if (window.confirm(`Удалить ${this.state.blogArr[pos].title}?`)) {
+      const temp = [...this.state.blogArr];
+      temp.splice(pos, 1);
+      this.setState({
+        blogArr: temp,
+      });
+      localStorage.setItem('blogPosts', JSON.stringify(temp));
     }
-    
-    
-  }
+  };
   handleAddFormShow = () => {
     this.setState({
       showAddForm: true,
-    })
-  }
+    });
+  };
 
   handleAddFormHide = () => {
     this.setState({
       showAddForm: false,
-    })
-  }
-    
-      render() {
-        const blogPosts = this.state.blogArr.map((item, pos) => {
-          return (
-            <BlogCard
-              key={item.id}
-              title={item.title}
-              description={item.description}
-              liked={item.liked}
-              likePost={() => this.likePost(pos)}
-              deletePost={() => this.deletePost(pos)}
-            />
-          )
-        })
-        return (
-          
-          <>
-          {this.state.showAddForm && <AddPostForm handleAddFormHide={this.handleAddFormHide} />}
-         
-            
-            <h1>Simple Blog</h1>
-            <button onClick={this.handleAddFormShow} className='blackBtn'>Создать пост</button>
-          <div className="posts">
-            {blogPosts}
-          </div>
-          </>
-        )
-}
-          
+    });
+  };
+
   
-          
+  addNewBlogPost = (blogPost) => {
+    this.setState((state) => {
+      const posts = [...state.blogArr]
+      posts.push(blogPost)
+      localStorage.setItem('blogPosts', JSON.stringify(posts));
+      return {
+        blogArr: posts
+      }
+    })
+
+  }
+
+  handleEscape = (e) => {
+    if (e.key === 'Escape' && this.state.showAddForm) {
+      this.handleAddFormHide();
+    }
+  };
+
+
+  componentDidMount() {
+    axios.get("https://629a0d587b866a90ec486a69.mockapi.io/posts").then((response) => {
+      this.setState({
+        blogArr: response.data
+      })
+    })
+
+    window.addEventListener('keyup', this.handleEscape);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleEscape);
+  }
+
+  render() {
+    const blogPosts = this.state.blogArr.map((item, pos) => {
+      return (
+        <BlogCard
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          liked={item.liked}
+          likePost={() => this.likePost(pos)}
+          deletePost={() => this.deletePost(pos)}
+        />
+      );
+    });
+    return (
+      <>
+        {this.state.showAddForm && (
+          <AddPostForm addNewBlogPost={this.addNewBlogPost} blogArr={this.state.blogArr} handleAddFormHide={this.handleAddFormHide} />
+        )}
+
+        <h1>Simple Blog</h1>
+        <button onClick={this.handleAddFormShow} className="blackBtn">
+          Создать пост
+        </button>
+        <div className="posts">{blogPosts}</div>
+      </>
+    );
+  }
 }
